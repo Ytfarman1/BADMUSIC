cd29e7879e1f1ee77bc0fa15e3af4b57c1a00188
Ytfarman1 <farmanansari9698@gmail.com>
2025-11-08 15:20:04 +0530

    Update thumbnails.py

--- BADMUSIC/utils/thumbnails.py
+++ BADMUSIC/utils/thumbnails.py
@@ -125,7 +125,7 @@ async def get_thumb(videoid: str) -> str:
 
     # Add "PURVI BOTS" top-right (default font)
     font = ImageFont.truetype("BADMUSIC/assets/font.ttf", 28)  # 
-    text = "BAD MUSIC "
+    text = "YT MUSIC"
     text_size = draw.textsize(text, font=font)
     draw.text((1280 - text_size[0] - 10, 10), text, fill="yellow", font=font)
     # Cleanup and save
